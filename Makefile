e0ad2d925da95ce5e472eb8096a0e16a49585eeb
recroot <recroot89@gmail.com>
2019-03-23 17:11:15 +0500

    CHECKPOINT 1

---
+++ Makefile
@@ -0,0 +1,1 @@
+include makefile-app.mk
---
+++ makefile-app.mk
@@ -0,0 +1,16 @@
+USER = "$(shell id -u):$(shell id -g)"
+
+app:
+	docker-compose up
+
+app-setup: app-build
+	docker-compose run --user=$(USER) app bundle install
+
+app-build:
+	docker-compose build
+
+app-bash:
+	docker-compose run --user=$(USER) app bash
+
+app-db-prepare:
+	docker-compose run --user=$(USER) app bin/rails db:migrate db:fixtures:load
