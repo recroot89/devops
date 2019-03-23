be3cdb8b798c24c38f507a18020d7544c714c58b
recroot <recroot89@gmail.com>
2019-03-24 01:28:42 +0500

    switch to minitest

--- services/app/test/test_helper.rb
+++ services/app/test/test_helper.rb
@@ -1,30 +1,30 @@
+# frozen_string_literal: true
+
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
-require 'minitest/reporters'
-Minitest::Reporters.use!
+require 'minitest/profile'
+require 'minitest/autorun'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
-  
+
   # Returns true if a test user is logged in.
   def is_logged_in?
     !session[:user_id].nil?
   end
-  
+
   # Log in as a particular user.
   def log_in_as(user)
     session[:user_id] = user.id
   end
 
-
   # Add more helper methods to be used by all tests here...
 end
 
 class ActionDispatch::IntegrationTest
-
   # Log in as a particular user.
   def log_in_as(user, password: 'password', remember_me: '1')
     post login_path, params: { session: { email: user.email,
